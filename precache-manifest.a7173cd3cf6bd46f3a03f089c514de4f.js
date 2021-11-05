7f90ff008e582aae92b831ee13879d931c51f9ea
Srikar Ayyalasomayajula <srikar7a@gmail.com>
2021-11-05 15:34:46 -0400

    Updates

---
+++ precache-manifest.a7173cd3cf6bd46f3a03f089c514de4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4afa2c666b1429cfe94d6522b8c492",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "ba19a286a9ed0d1032f8",
+    "url": "/website/static/css/main.d9253614.chunk.css"
+  },
+  {
+    "revision": "b5c919dbcf5ee02fb046",
+    "url": "/website/static/js/2.911b6e9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/website/static/js/2.911b6e9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba19a286a9ed0d1032f8",
+    "url": "/website/static/js/main.323c54cf.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "300bc8159907a87de9302873289909ab",
+    "url": "/website/static/media/profPic.300bc815.png"
+  },
+  {
+    "revision": "67c99fd238c40826af4ef0024f2d94b8",
+    "url": "/website/static/media/srikarResume.67c99fd2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.843a8d89ccf2b5b0e4011975ecfbc5f3.js"
+  "/website/precache-manifest.a7173cd3cf6bd46f3a03f089c514de4f.js"
 );
 
 self.addEventListener('message', (event) => {
